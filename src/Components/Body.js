87ab962d5e19bd1b713d0e86a93464ba5a70c431
mạnh tân <manhtanqhp@gmail.com>
2022-10-02 14:59:27 +0700

    first commit

---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import imagess from '../img/rapphim.png';
+import "../css/style.css"
+import "../css/gozha-nav.css"
+
+export default function body() {
+  return (
+        <div className="bannercontainer">
+                        <div className="banner">
+                             <img alt='' src={imagess}/>
+                        </div>
+                    </div>
+                  
+  )
+}
